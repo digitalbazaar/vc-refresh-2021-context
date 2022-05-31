7d1dbc3ec63c8676440d99fe7b58048523c0bc3f
Andrew Jones <ajones@digitalbazaar.com>
2022-05-31 14:57:46 -0400

    Add context to js side.

--- js/context.js
+++ js/context.js
@@ -7,7 +7,11 @@ module.exports = {
   '@context': {
     id: '@id',
     type: '@type',
-    '@protected': true
-
+    '@protected': true,
+    cred: 'https://www.w3.org/2018/credentials#',
+    validAfter: 'cred:validAfter',
+    validUntil: 'cred:validUntil',
+    MediatedRefresh2021: 'cred:MediatedRefresh2021',
+    UnmediatedRefresh2021: 'cred:UnmediatedRefresh2021'
   }
 };
