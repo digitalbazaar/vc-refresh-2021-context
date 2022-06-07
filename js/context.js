d98b3da02a5815f1adc06a5859ae61a0a8b7ec4a
Andrew Jones <ajones@digitalbazaar.com>
2022-06-07 14:02:22 -0400

    Remove version for refresh2021 term & use schema.org for url.

--- js/context.js
+++ js/context.js
@@ -9,8 +9,8 @@ module.exports = {
     {
       id: '@id',
       type: '@type',
-      refresh2021: 'https://w3id.org/vc-refresh-2021/v1#',
-      url: 'refresh2021:url',
+      refresh2021: 'https://w3id.org/vc-refresh-2021#',
+      url: 'http://schema.org/url',
       validAfter: {'@id': 'refresh2021:validAfter', '@type': 'xsd:dateTime'},
       validUntil: {'@id': 'refresh2021:validUntil', '@type': 'xsd:dateTime'},
       MediatedRefresh2021: 'refresh2021:MediatedRefresh2021',
